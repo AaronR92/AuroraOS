a2e5af1602c7a9de4f6a260a111f4e105d106d14
Anton Ruzanov <antonru924@gmail.com>
2023-09-10 17:04:37 +0300

    Switching to 64-bit. Printing OKAY instead of OK 😁

--- src/impl/x86_64/boot/main.asm
+++ src/impl/x86_64/boot/main.asm
@@ -1,4 +1,5 @@
 global start
+extern long_mode_start
 
 section .text
 bits 32
@@ -9,16 +10,18 @@ start:
     call check_cpuid ; check for cpuid instruction (provides cpu info)
     call check_long_mode ; check for long mode support
 
-    ; print `OK`
-    mov dword [0xb8000], 0x2f4b2f4f
-    ; freeze cpu
+    call setup_page_tables
+    call enable_paging
+
+    lgdt [gdt64.pointer]
+    jmp gdt64.code_segment:long_mode_start
+
     hlt
 
 check_multiboot:
     cmp eax, 0x36d76289
     jne .no_multiboot
     ret
-
 .no_multiboot:
     mov al, "M" ; M = multiboot error
     jmp error
@@ -37,7 +40,6 @@ check_cpuid:
 	cmp eax, ecx
 	je .no_cpuid
 	ret
-
 .no_cpuid:
     mov al, "C"  ; C = cpu error
     jmp error
@@ -54,10 +56,55 @@ check_long_mode:
 	jz .no_long_mode
 
 	ret
-
 .no_long_mode:
     mov al, "L" ; L = long mode error
     jmp error
+
+setup_page_tables:
+	mov eax, page_table_l3
+	or eax, 0b11 ; present, writable
+	mov [page_table_l4], eax
+
+	mov eax, page_table_l2
+	or eax, 0b11 ; present, writable
+	mov [page_table_l3], eax
+
+	mov ecx, 0 ; counter
+.loop:
+    ; mapping p2 table
+	mov eax, 0x200000 ; 2MiB
+	mul ecx
+	or eax, 0b10000011 ; present + writable + huge
+	mov [page_table_l2 + ecx * 8], eax ; map ecx-th entry
+
+	inc ecx ; increment counter
+	cmp ecx, 512 ; checks if the whole table is mapped
+	jne .loop ; if not, continue
+
+	ret
+
+enable_paging:
+    ; load P4 to cr3 register (cpu uses this to access the P4 table)
+    mov eax, page_table_l4
+    mov cr3, eax
+
+    ; enable PAE-flag in cr4 (Physical Address Extension)
+    mov eax, cr4
+    or eax, 1 << 5
+    mov cr4, eax
+
+    ; set the long mode bit in the EFER MSR (model specific register)
+    mov ecx, 0xC0000080
+    rdmsr
+    or eax, 1 << 8
+    wrmsr
+
+    ; enable paging in the cr0 register
+    mov eax, cr0
+    or eax, 1 << 31
+    mov cr0, eax
+
+    ret
 
 error:
     ; print "ERR: X" where X is the error code
@@ -68,7 +115,22 @@ error:
     hlt
 
 section .bss
+align 4096
+page_table_l4:
+    resb 4096
+page_table_l3:
+    resb 4096
+page_table_l2:
+    resb 4096
 stack_bottom:
     resb 4096 * 4 ; reserving 16KB of memory for stack
-
 stack_top:
+
+section .rodata
+gdt64:
+	dq 0 ; zero entry
+.code_segment: equ $ - gdt64
+	dq (1 << 43) | (1 << 44) | (1 << 47) | (1 << 53) ; code segment
+.pointer:
+	dw $ - gdt64 - 1 ; length
+	dq gdt64 ; address
